1afb0adc3c956fed403bf36b722f8179c2671f04
miasolveig <32290529+miasolveig@users.noreply.github.com>
2017-12-01 12:23:05 +0100

    Update index.php
    grund skelet

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-!docty
+<?php
+    session_start();
+?>
+<!doctype html>
+<html lang="da">
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scales=1">
+      <meta name="description" content="Description">
+      <meta name="robots" content="index, follow">
+        <meta property="og:title" content="Title"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="URL"/>
+        <meta property="og:image" content="Image"/>
+        <meta property="og:description" content="Description"/>
+        <meta property="og:locale" content="da_DK"/>
+      <title>Det Rigtige Match</title>
+      <link href="css/style.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="js/script.js"></script>
+  </body>
+</html>
