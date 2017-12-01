1f19c02d488bfc37f51d49522c3923793898a2ce
miasolveig <32290529+miasolveig@users.noreply.github.com>
2017-12-01 13:01:56 +0100

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect($db_host="localhost",$db_user="root",$db_password="",$db_database="php-undervisning")
+$db = mysqli_connect($db_host="localhost",$db_user="root",$db_password="",$db_database="perfekt_match")
 or die("Error - cant connect or find db");
 mysqli_set_charset($db,"utf8")
 ?>
