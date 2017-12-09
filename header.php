dac84123616b4e2f715b9e74da0187a46d15ffb9
miasolveig <32290529+miasolveig@users.noreply.github.com>
2017-12-09 14:37:48 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,3 +1,25 @@
 <header>
-
+    <div id="login">
+        <a>Log ind</a>
+        <a>Min side</a>
+        <div id="admin">
+            <a>Hidden link - som kun dukker op hvis man er logget ind som admin</a>
+        </div>
+    </div>
+    <div class="mobile">
+        <span id="navBars" class="fa fa-bars"></span>
+    </div>
+    <img src="img" alt="Det Rigtige Match! logo">
+    <nav>
+        <div id="elements">
+            <a href="index.php">Forside</a>
+            <a href="aktiviteter.php">Aktiviteter</a>
+            <a href="defrivillige.php">De Frivillige</a>
+            <a href="omos.php">Om os</a>
+            <a href="blivfrivillig.php" type="button">Bliv frivillig</a>
+        </div>
+    </nav>
+    <div class="mobile">
+        <input type="search" placeholder="Søg på en aktivitet">
+    </div>
 </header>
---
+++ minside.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scales=1">
+    <meta name="description" content="Description">
+    <meta name="robots" content="noindex, nofollow">
+    <title>Det Rigtige Match! Min side</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php include 'header.php'
+?>
+<div>
+    <h1>
+        Det Rigtige Match!
+    </h1>
+    <h2>
+
+    </h2>
+</div>
+<?php include 'sidenav.php'
+?>
+<?php include 'footer.php'
+?>
+<!-- JQuery så det kan loades -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
