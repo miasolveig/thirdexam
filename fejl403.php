0fc91633fdc669e5511a8cfc436c378dc49048fe
Kamillameyer95 <34159944+Kamillameyer95@users.noreply.github.com>
2017-12-20 18:41:58 +0100

    Add files via upload

---
+++ fejl403.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="da">
+<head>
+    <!-- typefacen på din hjemmeside -->
+    <meta charset="utf-8">
+    <!-- Responsiv design -->
+    <meta name="viewport" content="width=device-width, initial-scales=1">
+    <!-- Max ca 160 karakterer -->
+    <meta name="description" content="beskrivelse">
+    <!-- Få unikke og relevante (Skal gå igen på siden)-->
+    <meta name="keywords" content="keywords">
+    <!-- Indeksere siden / følge links på siden -->
+    <meta name="robots" content="index, follow">
+
+    <!-- OpenGraph bruges når din hjemmeside bliver delt på sociale medier -->
+    <meta property="og:title" content="relevant title"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="http://www.domain.dk"/>
+    <meta property="og:image" content="logo eller relevant billede"/>
+    <meta property="og:description" content="forklaring"/>
+    <meta property="og:locale" content="da_DK"/>
+
+    <!--Faneblads navn -->
+    <title>Untitled Document</title>
+    <!-- link til css fil -->
+    <link href="Css/fejl403.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<main>
+    <h1>UPS!</h1>
+    <p>
+        Denne side har du ikke adgang til vælg venligst en anden i hovede menuen
+    </p>
+</main>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kamilla
+ * Date: 20-12-2017
+ * Time: 18:31
+ */
+?>
+
+<!-- JQuery så det kan loades -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
+
+
