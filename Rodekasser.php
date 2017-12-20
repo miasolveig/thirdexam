bde0b7d81ebc1a002aad7316dc0abbf4ed260833
Kamillameyer95 <34159944+Kamillameyer95@users.noreply.github.com>
2017-12-20 16:00:42 +0100

    Add files via upload

---
+++ Rodekasser.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="da">
+<head>
+    <!-- typefacen på din hjemmeside -->
+    <meta charset="utf-8">
+    <!-- Responsiv design -->
+    <meta name="viewport" content="width=device-width, initial-scales=1">
+    <!-- Max ca 160 karakterer -->
+    <meta name="description" content="beskrivelse">
+    <!-- Få unikke og relevante (Skal gå igen på siden)-->
+    <meta name="keywords" content="keywords">
+    <!-- Indeksere siden / følge links på siden -->
+    <meta name="robots" content="index, follow">
+
+    <!-- OpenGraph bruges når din hjemmeside bliver delt på sociale medier -->
+    <meta property="og:title" content="relevant title"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="http://www.domain.dk"/>
+    <meta property="og:image" content="logo eller relevant billede"/>
+    <meta property="og:description" content="forklaring"/>
+    <meta property="og:locale" content="da_DK"/>
+
+    <!--Faneblads navn -->
+    <title>Untitled Document</title>
+    <!-- link til css fil -->
+    <link href="Css/Rodekasser.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div>
+    <h2>Bliv frivillig</h2>
+    <h3>3 fordel ved at blive frivillig</h3>
+    <div id="kasser">
+        <div class="rod">
+            <h2>1</h2>
+            <p>Du bliver en del af et
+                fællesskab og får et
+                større netværk med
+                mange andre frivillige</p>
+        </div>
+        <div class="rod2">
+            <h2>2</h2>
+            <p>Du kommer til at lære
+                noget om dig selv, da
+                du bliver udfordret
+                både socialt og fysisk,
+                og får rykket nogle
+                grænsere</p>
+
+        </div>
+        <div class="rod3">
+            <h2>3</h2>
+            <p>Du får nye kompetencer
+                og kvalifikationer,
+                der kan skrives
+                på CV’et. Dette kan
+                hjælpe dig med at stå
+                stærkere i forhold til
+                uddannelse eller jobt</p>
+
+        </div>
+    </div>
+</div>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kamilla
+ * Date: 20-12-2017
+ * Time: 15:38
+ */
+?>
+
+<!-- JQuery så det kan loades -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
+
+
